432a2d1e98f9b56b0389dd40122b52caf24a9d00
kuldeep <kuldeeppaawar427@gmail.com>
2024-03-16 00:54:01 +0530

    s

---
+++ MadScientist.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back      
+#define tp3 1000
+#define tp5 100000
+#define tp6 1000000
+#define tp9 1000000000
+#define inf 100000000000000000
+#define mod 1000000007
+#define ff first
+#define ss second
+
+
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef map<ll,ll> mll;
+typedef stack<ll> sll;
+typedef stack<pll> spll;
+typedef greater<ll> gll;
+typedef priority_queue<ll,vll,gll> pqmin;
+typedef priority_queue<ll> pqmax;
+
+
+
+
+/**          HINTS/NOTES                 **/
+//when a problem statement says to find the end result of some
+//process, or to find when something occurs, it’s usually sufficient to simulate the process.
+//binary exponentiation - use pow()function for finding powers does it much faster 
+//A bitmask is an integer whose binary 
+//representation is used to represent a subset. 
+
+
+
+
+
+
+
+
+
+ll int gcd_(ll int a,ll int b){ //eculidean algo for finding gcd 
+    while(a){
+        ll t =a;
+        a=b%a;
+        b=t;
+    }
+    return b;
+}
+ll int lcm__(ll int gcd,ll int a,ll int b){
+    ll int t = (a*b);
+    ll int g =gcd_(a,b);
+    return t/g;
+    //gcd * lcm = a*b;
+    //min(x^a,x^b) for every prime factor = gcd(a,b)
+    //max(x^a,x^b) for every prime factor = lcm(a,b)
+}
+
+
+void debug(int a){
+    cout<<a;
+}
+
+
+
+void fun(){
+   
+};
+
+
+int main(){
+    freopen("breedflip.in", "r", stdin);
+	// the following line creates/overwrites the output file
+	freopen("breedflip.out", "w", stdout);
+    
+    int n,ans=0;
+    cin>>n;
+    string a,b;
+    cin>>a>>b;
+    
+    for(int i=0;i<n-1;i++){
+        if(a[i]!=b[i] &&a[i+1]==b[i+1] ){
+           ans++;
+        }
+       
+           
+         
+        
+    }
+    cout<<ans;
+    
+    
+
+
+    return 0;
+
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,3 @@
-    // cout<<x<<endl;
+ // freopen("breedflip.in", "r", stdin);
+	// // the following line creates/overwrites the output file
+	// freopen("breedflip.out", "w", stdout);
